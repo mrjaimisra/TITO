8077db32c5b69813d0f2b07a4f42e9cf6590c091
Jai Misra <misrajai01@gmail.com>
2024-09-09 04:50:45 -0600

    Adding some logging to the script runner to show when it is pulling from git writing to a file, and saving records in the database

--- lib/run.rb
+++ lib/run.rb
@@ -5,15 +5,23 @@ require './app/models/changed_file.rb'
 # app.rb
 require "sinatra/activerecord"
 
-puts "Pulling from git and writing the output to a file...\n\n"
-file = GitPullFileWriter.new(path_to_project: ARGV[0]).pull
-if !file
-  puts "Already up to date."
-  exit
+passed_in_file_path = ARGV[1]
+if passed_in_file_path
+  puts "Parsing the output of #{passed_in_file_path}...\n\n"
+  file = File.open(passed_in_file_path)
+  output = file.read
+else
+  puts "Pulling from git and writing the output to a file...\n\n"
+  file = GitPullFileWriter.new(path_to_project: ARGV[0]).pull
+  if !file
+    puts "Already up to date."
+    exit
+  end
+  output = File.open(file.path).read
 end
-output = File.open(file.path).read
 file.close
 puts output
 parser = GitPullParser.new
 parser.parse(output)
-puts parser.save_changed_files!
+puts "\n\nCreating #{parser.changed_files.length} records in the database...\n\n"
+parser.save_changed_files!
