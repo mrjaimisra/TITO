211c7be60e2281e7d04d3c73ae6218c6403ddcfa
Jai Misra <misrajai01@gmail.com>
2024-09-16 10:15:21 -0600

    Disable retries for the Sidekiq job created in GitPullWorker

--- app/workers/git_pull_worker.rb
+++ app/workers/git_pull_worker.rb
@@ -13,6 +13,7 @@ end
 
 class GitPullWorker
   include Sidekiq::Job
+  sidekiq_options retry: false # job will be discarded if it fails
 
   def perform(path_to_project = ENV["PATH_TO_PROJECT"], path_to_file = nil)
     runner = Runner.new(path_to_project:, path_to_file:)
